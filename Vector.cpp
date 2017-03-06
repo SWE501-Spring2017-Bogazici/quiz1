b884e1588159ffb42b6f7befa9673ed3eca43d16
Vedat Akses <vedatakses@gmail.com>
2017-03-06 20:09:41 +0300

    Change operator *= implementation

--- Vector.cpp
+++ Vector.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -7,7 +7,7 @@ using namespace std;
 struct Vect {
     int size;
     int *array;
-
+    
     Vect(int n) {
         size = n;
         array = new int[size];
@@ -15,11 +15,11 @@ struct Vect {
             array[i] = 0;
         }
     }
-
+    
     ~Vect() {
-        cout << "Vect destructed.."<< endl;
+        cout << "Vect number destructed.."<< endl;
     }
-
+    
     void random(int min, int max)
     {
         for (int i = 0; i < size; i++) {
@@ -27,67 +27,72 @@ struct Vect {
             array[i] = rand;
         }
     }
-
-    void operator*=(Vect& z) {
+    
+    int operator*=(Vect& z) {
+        int val = 0;
+        
         if (this->size != z.size) {
             cout << "Arrays size are not equal! " << endl;
+        } else {
+            for (int i = 0; i < size; i++) {
+                val += array[i] * z.array[i];
+            }
         }
-        for (int i = 0; i < size; i++) {
-            array[i] *= z.array[i];
-        }
+        return val;
     }
-
+    
     void operator+=(Vect& z) {
         if (this->size != z.size) {
             cout << "Arrays size are not equal! " << endl;
-        }
-        for (int i = 0; i < size; i++) {
-            array[i] += z.array[i];
+        } else {
+            for (int i = 0; i < size; i++) {
+                array[i] += z.array[i];
+            }
         }
     }
-
+    
     void operator-=(Vect& z) {
         if (this->size != z.size) {
             cout << "Arrays size are not equal! " << endl;
-        }
-        for (int i = 0; i < size; i++) {
-            array[i] -= z.array[i];
+        } else {
+            for (int i = 0; i < size; i++) {
+                array[i] -= z.array[i];
+            }
         }
     }
-
+    
 };
 
-// left shift operator overloading for Vector
+// left shift operator overloading for Complex Numbers
 ostream& operator<<(ostream& out, Vect& z) {
-    cout << "vector: ";
     for (int i = 0; i < z.size; i++) {
         out << z.array[i] << " ";
     }
-
+    
     return out;
 }
 
 int main() {
-
+    
     // Initializing v1
     Vect v1(10);
     v1.random(0, 50);
-
+    
     // Initializing v2
     Vect v2(10);
     v2.random(0, 50);
-
+    
     cout << "v1: " << v1 << endl;
     cout << "v2: " << v2 << endl;
-
+    
     v1 += v2;
     cout << "v1 += v2 : " << v1 << endl;
-
-    v1 *= v2;
-    cout << "v1 *= v2 :" << v1 << endl;
-
+    
+    int mult = v1 *= v2;
+    cout << "v1 *= v2 : " << mult << endl;
+    
     v1 -= v2;
-    cout << "v1 -= v2 :" << v1 << endl;
-
+    cout << "v1 -= v2 : " << v1 << endl;
+    
     return 0;
 }
